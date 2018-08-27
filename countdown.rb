c3b84d2eb25fbb316f46ed00bb51ff29dc78443a
Nick Romans <nicholas.romans@gmail.com>
2018-08-27 17:34:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,10 @@ def countdown(integer)
   while integer > 0 
   integer -= 1
   puts "#{integer} SECOND(S)!"
+  countdown_with_sleep(integer)
 end
 
 def countdown_with_sleep(integer)
   until integer == 0 
-  
+  sleep 1 
+end
